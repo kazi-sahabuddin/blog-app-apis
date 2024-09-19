238433726b56aa38739cf69340a4548841c06293
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-09-19 23:16:06 +0600

    feat(user):update detele

--- src/main/java/com/sahabuddin/blogappapis/controllers/UserController.java
+++ src/main/java/com/sahabuddin/blogappapis/controllers/UserController.java
@@ -5,10 +5,9 @@ import com.sahabuddin.blogappapis.services.UserService;
 import com.sahabuddin.blogappapis.services.impl.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -24,5 +23,17 @@ public class UserController {
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
         UserDto userDtoSaved = userService.createUser(userDto);
         return new ResponseEntity<>(userDtoSaved, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") Long userId, @RequestBody UserDto userDto) {
+        UserDto updateUser = userService.updateUser(userDto, userId);
+        return ResponseEntity.ok(updateUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable("userId") Long userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.ok(Map.of("message", "User deleted"));
     }
 }
